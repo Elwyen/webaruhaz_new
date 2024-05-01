58946098ccf3a2c22d1eff70b1c3ca860c63199f
SzalokiKlaudia <szaloki.klaudia90@gmail.com>
2024-05-01 20:43:10 +0200

    Kommentek inicializálása

---
+++ pages/termekek/kommentek.js
@@ -0,0 +1,59 @@
+const COMMENTS = [
+    {
+        id: '0',
+        product_id: 1,
+        comment: 'Delicious cakes, cupcakes, and cake balls! I love that you can customize the cupcake flavors and fillings to create just the cupcake you are craving!',
+        name: 'Suzy',
+    },
+    {
+        id: '1',
+        product_id: 1,
+        comment: 'Highly recommend it!!!',
+        jovalasz: 'Négyszeresükre',
+        name: 'Daniel',
+    },
+    {
+        id: '2',
+        product_id: 2,
+        comment: 'This place has the best desserts I have ever had.',
+        name: 'Patty',
+    },
+    {
+        id: '3',
+        product_id: 2,
+        comment: 'Ordered regular and gluten free cupcakes several times and everything has always been amazing. Delicious cupcakes no matter what flavor.',
+        name: 'Elen',
+    },
+    {
+        id: '4',
+        product_id: 3,
+        comment: 'Amazing !! Ordering again for my daughters birthday now !',
+        name: 'Vic',
+    },
+    {
+        id: '5',
+        product_id: 3,
+        comment: 'The service was not satisfying. I give this place only 2 stars',
+        name: 'Josh',
+    },
+];
+
+// document.addEventListener("DOMContentLoaded", (event) => {
+    
+// });
+
+// const products = document.querySelectorAll('.product'); 
+
+// products.forEach((product) => {
+//     product.addEventListener('click', (event) => { 
+//         const clickedProductId = event.target.id; // amire kattintok class termék annak az id-ját eltároljuk
+//         console.log('Clicked Product ID:', clickedProductId); // az id-et ellenőrzöm
+
+//         // Most, hogy megvan az id megjelenítjük a hozzátartozó hozzászólásokat
+
+//         // Komment gombhoz klikk eseményt rendelünk
+
+//         // Klikk esemény hatására kibővítjük a COMMENTS tömböt
+//     });
+// });
+
---
+++ pages/termekek/termekAdatok.js
@@ -0,0 +1,42 @@
+const termekek=[
+    {
+        nev:"Macaron",
+        id:0,
+        termekleiras: "xd",
+        ertekeles: 4,
+        ar: 15623,
+        kep: `../../public/kepek/termekek/kep0.jpg`
+    },
+    {
+        nev:"Rozsaszin nyomat",
+        id:1,
+        termekleiras: "xd",
+        ertekeles: 2,
+        ar: 12223,
+        kep: `../../public/kepek/termekek/kep1.jpg`
+    },
+    {
+        nev:"Rozsaszin csokis keksz",
+        id:2,
+        termekleiras: "xd",
+        ertekeles: 5,
+        ar: 1233,
+        kep: `../../public/kepek/termekek/kep2.jpg`
+    },
+    {
+        nev:"Rozsaszin egerkakis linzer",
+        id:3,
+        termekleiras: "xd",
+        ertekeles: 4,
+        ar: 1222223,
+        kep: `../../public/kepek/termekek/kep3.webp`
+    },
+    {
+        nev:"kis kedves sutemeny",
+        id:4,
+        termekleiras: "xd",
+        ertekeles: 5,
+        ar: 126563,
+        kep: `../../public/kepek/termekek/kep4.webp`
+    }
+]
